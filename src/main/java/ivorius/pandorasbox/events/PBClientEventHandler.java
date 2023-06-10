d7b5ee7fabe43915db3b3ae85dfe860c8cf2e585
Alexandra-Myers <bia416cat@gmail.com>
2023-06-10 12:47:02 -0400

    more 1.20 work

--- src/main/java/ivorius/pandorasbox/events/PBClientEventHandler.java
+++ src/main/java/ivorius/pandorasbox/events/PBClientEventHandler.java
@@ -1,10 +1,28 @@
 package ivorius.pandorasbox.events;
 
 import ivorius.pandorasbox.PandorasBox;
+import ivorius.pandorasbox.client.rendering.PandorasBoxBlockEntityRenderer;
+import ivorius.pandorasbox.client.rendering.PandorasBoxRenderer;
+import ivorius.pandorasbox.init.Registry;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = PandorasBox.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class PBClientEventHandler {
-
+    @SubscribeEvent
+    public void clientInit(FMLClientSetupEvent event) {
+        EntityRenderers.register(Registry.Box.get(), PandorasBoxRenderer::new);
+        PandorasBoxBlockEntityRenderer.register();
+    }
+    @SubscribeEvent
+    public void addPB(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
+            event.accept(Registry.PB);
+        }
+    }
 }
