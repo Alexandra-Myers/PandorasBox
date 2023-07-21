2a497bdc29ddfc68add2c85e2cc1db4826c99728
Alexandra-Myers <bia416cat@gmail.com>
2023-07-21 16:02:59 -0400

    revert command until it works

--- src/main/java/ivorius/pandorasbox/commands/CommandPandorasBox.java
+++ src/main/java/ivorius/pandorasbox/commands/CommandPandorasBox.java
@@ -6,60 +6,42 @@
 package ivorius.pandorasbox.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.BoolArgumentType;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import ivorius.brigadieropts.commands.ArgumentName;
-import ivorius.brigadieropts.commands.ExpectingCommand;
 import ivorius.pandorasbox.effectcreators.PBECRegistry;
 import ivorius.pandorasbox.effectcreators.PBEffectCreator;
 import ivorius.pandorasbox.entitites.PandorasBoxEntity;
 import ivorius.pandorasbox.utils.PBEffectArgument;
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.entity.Entity;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class CommandPandorasBox extends ExpectingCommand {
+public class CommandPandorasBox
+{
 
     public CommandPandorasBox(CommandDispatcher<CommandSource> dispatcher) {
-        super(dispatcher);
+        register(dispatcher);
     }
-    @Override
-    public int execute(CommandSource source, Map<String, Object> args) {
-        Entity player;
-        try {
-            player = source.getPlayerOrException();
-        } catch (CommandSyntaxException e) {
-            throw new RuntimeException(e);
-        }
-        PBEffectCreator effectCreator = null;
-        boolean bool = false;
-
-        if (args.containsKey("player")) {
-            player = (Entity) args.get("player");
-        }
-        if (args.containsKey("effect")) {
-            effectCreator = (PBEffectCreator) args.get("effect");
-        }
-        if (args.containsKey("invisible")) {
-            bool = (boolean) args.get("invisible");
-        }
-        assert player != null;
+    static void register(CommandDispatcher<CommandSource> dispatcher) {
+        dispatcher.register(Commands.literal("pandora")
+                .requires(cs->cs.hasPermission(2)) //permission
+                .then(Commands.argument("player", EntityArgument.player())
+                        .executes(ctx -> createBox(EntityArgument.getPlayer(ctx, "player"), null, false)).then(Commands.argument("effect", PBEffectArgument.effect())
+                                .executes(ctx -> createBox(EntityArgument.getPlayer(ctx, "player"), PBEffectArgument.getEffect(ctx, "effect"), false)).then(Commands.argument("invisible", BoolArgumentType.bool())
+                                        .executes(ctx -> createBox(EntityArgument.getPlayer(ctx, "player"), PBEffectArgument.getEffect(ctx, "effect"), BoolArgumentType.getBool(ctx, "invisible")))))
+                ));
+    }
+    public static int createBox(Entity player, PBEffectCreator effectCreator, boolean bool) {
         PandorasBoxEntity box;
 
         if (effectCreator != null)
         {
             box = PBECRegistry.spawnPandorasBox(player.level, player.getCommandSenderWorld().random, effectCreator, player);
-
             if (box != null)
                 box.setCanGenerateMoreEffectsAfterwards(false);
         }
         else
             box = PBECRegistry.spawnPandorasBox(player.level, player.getCommandSenderWorld().random, true, player);
-
         if (box != null)
         {
             if (bool)
@@ -69,27 +51,6 @@ public class CommandPandorasBox extends ExpectingCommand {
             }
         }
         return 1;
-    }
-    @Override
-    public int permissionLevel() {
-        return 2;
-    }
-    @Override
-    public String commandName() {
-        return "pandora";
-    }
-    @Override
-    public Map<ArgumentName, ArgumentType<?>> typeMap() {
-        Map<ArgumentName, ArgumentType<?>> map = new HashMap<>();
-        map.put(new ArgumentName("player", false, false, ((commandSourceCommandContext, s) -> {
-            try {
-                return EntityArgument.getPlayer(commandSourceCommandContext, s);
-            } catch (CommandSyntaxException e) {
-                throw new RuntimeException(e);
-            }
-        })), EntityArgument.player());
-        map.put(new ArgumentName("effect", false, false, PBEffectArgument::getEffect), PBEffectArgument.effect());
-        map.put(new ArgumentName("invisible", false, false, BoolArgumentType::getBool), BoolArgumentType.bool());
-        return map;
+
     }
 }
