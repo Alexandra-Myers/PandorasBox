b3a65b6757d254471d6e16a98701f55df46c1ebd
Alexandra-Myers <bia416cat@gmail.com>
2023-07-27 21:10:28 -0400

    2.3.5

--- src/main/java/ivorius/pandorasbox/utils/StringConverter.java
+++ src/main/java/ivorius/pandorasbox/utils/StringConverter.java
@@ -1,5 +1,8 @@
 package ivorius.pandorasbox.utils;
 
+import java.util.List;
+import java.util.Objects;
+
 public class StringConverter {
     private StringConverter() {
 
@@ -7,36 +10,18 @@ public class StringConverter {
     public static String convertCamelCase(String id) {
         int firstUpper;
         final int len = id.length();
-        char[] charsSeen = new char[5];
-        int[] charLocations = new int[5];
 
         /* Now check if there are any characters that need to be changed. */
-        for (int it = 0; it < 3; it++) {
+        while (!Objects.equals(id, id.toLowerCase())) {
             scan:
             {
-                for (firstUpper = 0; firstUpper < len; ) {
+                for (firstUpper = 0; firstUpper < len; firstUpper++) {
                     char c = id.charAt(firstUpper);
-                    if(c == charsSeen[0] && c == charLocations[0]) {
-                        firstUpper++;
-                        continue;
-                    }
-                    if(c == charsSeen[1] && c == charLocations[1]) {
-                        firstUpper++;
-                        continue;
-                    }
-                    if(c == charsSeen[2] && c == charLocations[2]) {
-                        firstUpper++;
-                        continue;
-                    }
                     if (c != Character.toLowerCase(c)) {
                         String[] split = id.split(String.valueOf(c), 2);
                         id = split[0] + "_" + Character.toLowerCase(c) + split[1];
-                        charsSeen[it] = c;
-                        charLocations[it] = firstUpper;
-
                         break scan;
                     }
-                    firstUpper++;
                 }
             }
         }
