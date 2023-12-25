0a31fc36ff832eddd9a3cb92ebebe5f9e0e20198
Alexandra-Myers <bia416cat@gmail.com>
2023-12-24 20:32:24 -0500

    oh no

--- src/main/java/ivorius/pandorasbox/events/PBClientEventHandler.java
+++ src/main/java/ivorius/pandorasbox/events/PBClientEventHandler.java
@@ -5,21 +5,12 @@ import ivorius.pandorasbox.client.rendering.PandorasBoxBlockEntityRenderer;
 import ivorius.pandorasbox.client.rendering.PandorasBoxModel;
 import ivorius.pandorasbox.client.rendering.PandorasBoxRenderer;
 import ivorius.pandorasbox.init.Registry;
-import net.minecraft.network.syncher.EntityDataSerializer;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
-
-import java.util.Map;
-
-import static ivorius.pandorasbox.PandorasBox.logger;
 
 @Mod.EventBusSubscriber(modid = PandorasBox.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class PBClientEventHandler {
