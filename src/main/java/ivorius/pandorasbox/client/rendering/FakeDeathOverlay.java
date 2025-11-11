564e957f1ba5f244bd99099dd41105e659bb45d9
Alexandra-Myers <bia416cat@gmail.com>
2025-11-11 01:36:58 -0500

    Pandora's Box 2.6.0 1.21.9 Fabric - mmm

--- src/main/java/ivorius/pandorasbox/client/rendering/FakeDeathOverlay.java
+++ src/main/java/ivorius/pandorasbox/client/rendering/FakeDeathOverlay.java
@@ -42,7 +42,7 @@ public class FakeDeathOverlay extends Overlay {
     @Override
     public void render(GuiGraphics guiGraphics, int i, int j, float f) {
         float progress = (Util.getMillis() - startMillis) / 10000.0F;
-        if (minecraft.screen != null) minecraft.screen.render(guiGraphics, i, j, f);
+        if (minecraft.screen != null) minecraft.screen.renderWithTooltipAndSubtitles(guiGraphics, i, j, f);
         screen.renderWithTooltipAndSubtitles(guiGraphics, i, j, f);
         if (progress >= 0.7) {
             guiGraphics.drawCenteredString(screen.getFont(), Component.translatable("text.pandorasbox.fake", minecraft.getGameProfile().name()), screen.width / 2, 85, -1);
